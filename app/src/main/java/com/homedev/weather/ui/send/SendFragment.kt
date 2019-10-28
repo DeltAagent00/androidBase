d473fc9070ca5b91b20c80f9ad1cb498eb3c65bc
Alexandr Zheleznyakov <delta.agent00@gmail.com>
2019-10-28 13:03:09 +0300

    Добавил форму обратной связи

--- app/src/main/java/com/homedev/weather/ui/send/SendFragment.kt
+++ app/src/main/java/com/homedev/weather/ui/send/SendFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.homedev.weather.R
+import kotlinx.android.synthetic.main.fragment_send.*
 
 class SendFragment : Fragment() {
 
@@ -16,5 +18,22 @@ class SendFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_send, container, false)
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+
+    /** private **/
+
+
+    private fun initView() {
+        sendBtnView.setOnClickListener {
+            context?.let { itContext ->
+                Toast.makeText(itContext, R.string.message_success_send, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
