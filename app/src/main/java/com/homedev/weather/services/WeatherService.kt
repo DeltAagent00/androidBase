f62ee734c3cbf54985581f54ebd0cb638442b052
Alexandr Zheleznyakov <delta.agent00@gmail.com>
2019-11-14 15:35:52 +0300

    Урок 6

--- app/src/main/java/com/homedev/weather/services/WeatherService.kt
+++ app/src/main/java/com/homedev/weather/services/WeatherService.kt
@@ -2,10 +2,16 @@ package com.homedev.weather.services
 
 import android.app.IntentService
 import android.content.Intent
+import androidx.core.util.isEmpty
+import androidx.core.util.isNotEmpty
+import com.google.gson.Gson
 import com.homedev.weather.api.ApiWeather
 import com.homedev.weather.api.network.Result
 import com.homedev.weather.api.network.repositories.WeatherRepository
+import com.homedev.weather.api.response.WeatherResponse
 import com.homedev.weather.core.Constants
+import com.homedev.weather.core.db.DBHelper
+import com.homedev.weather.core.db.WeatherTable
 import com.homedev.weather.core.model.WeatherDataLoader
 import com.homedev.weather.utils.LoggerUtils
 import kotlinx.coroutines.CoroutineScope
@@ -17,6 +23,10 @@ import kotlinx.coroutines.launch
  * Created by Alexandr Zheleznyakov on 2019-10-31.
  */
 class WeatherService: IntentService("myService") {
+    companion object {
+        const val MIN_TIME_FOR_GET_FRESH_DATA = 60 * 60 * 1000L
+    }
+
     private val parentJob = Job()
     private val coroutineContext = parentJob + Dispatchers.Default
     private val scope = CoroutineScope(coroutineContext)
@@ -26,26 +36,70 @@ class WeatherService: IntentService("myService") {
             if (it.hasExtra(Constants.INTENT_TOWN)) {
                 val town = it.getStringExtra(Constants.INTENT_TOWN)
 
+                if (checkDataInBD(town)) {
+                    return
+                }
+
                 scope.launch {
                     val response = WeatherRepository.instance
                         .getWeatherForecast(town)
 
-                    val sendIntent = Intent(Constants.BROADCAST_RESPONSE_WEATHER)
-
                     when(response) {
                         is Result.Success -> {
-                            sendIntent.putExtra(Constants.INTENT_RESPONSE, response.data)
-                            sendIntent.putExtra(Constants.INTENT_CODE, ResponseCode.Success)
+                            saveDataTODB(town, response.data)
+                            sendResponse(response.data, ResponseCode.Success)
                         }
                         is Result.Error -> {
                             LoggerUtils.error(this, "getWeatherForecast error = " +
                                     response.exception.message)
-                            sendIntent.putExtra(Constants.INTENT_CODE, ResponseCode.Error)
+                            sendResponse(null, ResponseCode.Error)
                         }
                     }
-                    sendBroadcast(sendIntent)
+
                 }
             }
         }
+    }
+
+    private fun checkDataInBD(town: String): Boolean {
+        val dbHelper = DBHelper(baseContext).readableDatabase
+        val dataFromDB = WeatherTable.getAllDataFromCity(town, dbHelper)
+
+        val currentDate = System.currentTimeMillis()
+
+        if (dataFromDB.isNotEmpty() &&
+            (currentDate - dataFromDB.keyAt(0)) < MIN_TIME_FOR_GET_FRESH_DATA) {
+
+            val data = Gson().fromJson(dataFromDB.valueAt(0), WeatherResponse::class.java)
+            sendResponse(data, ResponseCode.Success)
+
+            return true
+        } else {
+            return false
+        }
+    }
+
+    private fun saveDataTODB(town: String, data: WeatherResponse) {
+        val dbHelper = DBHelper(baseContext).writableDatabase
+        val dataFromDB = WeatherTable.getAllDataFromCity(town, dbHelper)
+        val dataToString = Gson().toJson(data, WeatherResponse::class.java)
+
+        if (dataFromDB.isEmpty()) {
+            WeatherTable.addData(town, dataToString, dbHelper)
+        } else {
+            WeatherTable.editData(town, dataToString, dbHelper)
+        }
+    }
+
+    private fun sendResponse(data: WeatherResponse? = null, responseCode: ResponseCode) {
+        val sendIntent = Intent(Constants.BROADCAST_RESPONSE_WEATHER)
+
+        if (responseCode == ResponseCode.Success) {
+            sendIntent.putExtra(Constants.INTENT_RESPONSE, data)
+            sendIntent.putExtra(Constants.INTENT_CODE, ResponseCode.Success)
+        } else {
+            sendIntent.putExtra(Constants.INTENT_CODE, ResponseCode.Error)
+        }
+        sendBroadcast(sendIntent)
     }
 }
